8f8f7f433892eb25cc519ca71c194a5acc86ae92
fahras <fahras@clappingape.com>
2019-12-01 14:48:42 +0700

    Setup base

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
-let gulp = require("gulp"),
-    autoprefixer = require("gulp-autoprefixer"),
-    browserSync = require("browser-sync"),
-    webpack = require("webpack")
+let gulp           = require('gulp'),
+    sass           = require('gulp-sass'),
+    browserSync    = require('browser-sync'),
+    webpack        = require('webpack-stream'),
+    autoprefixer   = require('gulp-autoprefixer')
 
 // Compile css : "gulp sass"
 gulp.task('sass', () => {
-    return gulp.src('./src/assets/sass/**/*.scss')
+    return gulp.src('./resource/assets/sass/**/*.scss')
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(gulp.dest('./public/assets/css'))
@@ -13,23 +14,27 @@ gulp.task('sass', () => {
 
 // Webpack.config
 gulp.task('script', () => {
-    return gulp.src('./src/assets/javascripts/**/*.js')
+    return gulp.src('./resource/assets/js/**/*.js')
     .pipe(webpack(require('./webpack.config')))
     .pipe(gulp.dest('./public/assets/js/'))
 })
 
-// Start Server
-gulp.task("serve", () => {
+// Call serve : "gulp serve"
+gulp.task('serve', () => {
     browserSync.init({
-        server: {
-            baseDir: "./public/"
+        server : {
+            baseDir : './public/'
         }
     })
 
-    gulp.watch("./src/assets/javascripts/**/*.js", gulp.series('script'))
+    // Route
+    gulp.watch("./resource/assets/sass/**/*.scss", gulp.series('sass'))
+    gulp.watch("./resource/assets/js/**/*.js", gulp.series('script'))
 
+    // Browser Sync
     gulp.watch("./public/assets/js/**/*.js").on('change', browserSync.reload)
-    gulp.watch("./public/assets/image/**/*.svg").on('change', browserSync.reload)
     gulp.watch("./public/assets/css/**/*.css").on('change', browserSync.reload)
     gulp.watch("./public/*.html").on('change', browserSync.reload)
 })
+
+gulp.task('build', gulp.series(['sass', 'script']))
